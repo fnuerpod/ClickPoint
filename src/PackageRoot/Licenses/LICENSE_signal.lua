a7da6a46213f6046006d1088be50e65b89edaf3c
fnuer <88922673+fnuerpod@users.noreply.github.com>
2022-11-11 23:59:52 +0000

    chore: rename licenses folder to use capital letters.

--- src/PackageRoot/Licenses/LICENSE_signal.lua
+++ src/PackageRoot/Licenses/LICENSE_signal.lua
@@ -10,4 +10,4 @@ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR I
 
 ]]
 
-return "License for Sleitnick's Signal module."
+return "License for Sleitnick's Signal module"
